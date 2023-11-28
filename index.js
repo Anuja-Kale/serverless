0815c63b22265dedbf9aeb271f8f18075494f295
Anuja-Kale <anujakale16@gmail.com>
2023-11-28 14:13:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,76 +3,71 @@ const fetch = require('node-fetch');
 const { Storage } = require('@google-cloud/storage');
 const Mailgun = require('mailgun-js');
 const fs = require('fs').promises;
-const s3 = new AWS.S3();
 
-// Initialize AWS SDK services
+// AWS services initialization
+const s3 = new AWS.S3();
 const DynamoDB = new AWS.DynamoDB.DocumentClient();
 
-// Initialize Mailgun with environment variables
+// Mailgun initialization with environment variables
 const mailgun = Mailgun({
-  apiKey: process.env.MAILGUN_API_KEY, // Mailgun API key stored in Lambda environment variable
-  domain: process.env.MAILGUN_DOMAIN   // Mailgun domain stored in Lambda environment variable
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
 });
 
-// Lambda handler
 exports.handler = async (event) => {
+  // Parse the SNS message
   const snsMessage = JSON.parse(event.Records[0].Sns.Message);
 
-  // Define the S3 bucket and key name where the Google Cloud key file is stored
-  const bucketName = process.env.YOUR_S3_BUCKET_NAME; // Replace with your actual S3 bucket name
-  const keyName = process.env.YOUR_S3_KEY_NAME; // The name of your key file in S3
+  // Define the S3 bucket and key name for the Google Cloud key file
+  const bucketName = process.env.S3_BUCKET_NAME_FOR_GCS_KEY;
+  const keyName = process.env.S3_KEY_NAME_FOR_GCS_KEY;
 
-  // Define the temporary path for the key file
+  // Temporary path for the Google Cloud key file
   const tmpKeyFilePath = `/tmp/${keyName}`;
 
   try {
-    // Check if the key file exists in the /tmp directory
+    // Check if the key file exists in the /tmp directory, if not, get it from S3
     try {
       await fs.access(tmpKeyFilePath);
     } catch {
-      // Key file does not exist in /tmp, download it from S3
-      const params = {
-        Bucket: bucketName,
-        Key: keyName,
-      };
-      const data = await s3.getObject(params).promise();
+      // Key file is not in /tmp, download it from S3
+      const data = await s3.getObject({ Bucket: bucketName, Key: keyName }).promise();
       await fs.writeFile(tmpKeyFilePath, data.Body);
     }
 
-    // Initialize Google Cloud Storage with the key file from /tmp directory
+    // Initialize Google Cloud Storage with the key file
     const storage = new Storage({ keyFilename: tmpKeyFilePath });
-    const bucket = storage.bucket(process.env.GCS_BUCKET_NAME);
+    const gcsBucket = storage.bucket(process.env.GCS_BUCKET_NAME);
 
-    // URL of the GitHub release
+    // GitHub release URL
     const githubReleaseUrl = 'https://github.com/tparikh/myrepo/archive/refs/tags/v1.0.0.zip';
 
-    // Download the file from GitHub
+    // Download the GitHub release
     const response = await fetch(githubReleaseUrl);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch ${githubReleaseUrl}: ${response.statusText}`);
-    }
+    if (!response.ok) throw new Error(`Failed to fetch ${githubReleaseUrl}: ${response.statusText}`);
     const buffer = await response.buffer();
 
-    // Upload to Google Cloud Storage
+    // Upload the file to Google Cloud Storage
     const gcsFileName = 'myrepo-v1.0.0.zip';
-    const file = bucket.file(gcsFileName);
+    const file = gcsBucket.file(gcsFileName);
     await file.save(buffer);
 
-    // Send email notification
+    // Email notification data
     const emailData = {
-      from: `Your Name <${process.env.EMAIL_FROM}>`, // Sender's email
-      to: process.env.EMAIL_TO,                      // Recipient's email
+      from: `Your Name <${process.env.EMAIL_FROM}>`,
+      to: process.env.EMAIL_TO,
       subject: 'Download Status',
-      text: `Your download of ${gcsFileName} is completed and stored in Google Cloud Storage.`
+      text: `The download of ${gcsFileName} is complete and stored in Google Cloud Storage.`
     };
 
+    // Send email notification
     await mailgun.messages().send(emailData);
 
-    // Track email in DynamoDB
+    // Log the email sent in DynamoDB
     const emailRecord = {
-      TableName: process.env.DYNAMODB_TABLE,         // DynamoDB table name from environment variable
+      TableName: process.env.DYNAMODB_TABLE,
       Item: {
-        id: `email-${Date.now()}`,                   // Unique ID for the email record
+        id: `email-${Date.now()}`,
         to: emailData.to,
         from: emailData.from,
         subject: emailData.subject,
@@ -84,13 +79,13 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Download and email notification complete' }),
+      body: JSON.stringify({ message: 'Download and email notification complete.' })
     };
   } catch (error) {
     console.error('Error:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'An error occurred', error: error.message }),
+      body: JSON.stringify({ message: 'An error occurred', error: error.message })
     };
   }
 };
